866767e5f9f5ca3ea04cb3b765e2bc68919db3ed
Sushil Gurung <sushil.grg149@gmail.com>
2025-04-09 23:52:10 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Sushil Gurung',
+    author_email='sushil.grg149@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
